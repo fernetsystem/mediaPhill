652a8dc35268f167a38e1edf5b639a758f90ad66
Jth Rdz <fernetsystem@gmail.com>
2016-05-01 22:36:13 -0500

    crud de pacientes y doctores

--- controllers/controller.php
+++ controllers/controller.php
@@ -15,11 +15,23 @@ class controller{
         require_once 'views/main.php';        
         require_once 'views/footer.inc';
     }
+    public function closeSession(){	
+	session_destroy();
+	$myController = new controller();
+	$myController->Index();	
+    }
     public function registrarDoctor(){
         require_once 'views/headeri.inc';	
         require_once 'views/registrarDoctor.php';        
         require_once 'views/footer.inc';        
     }
+    
+    public function registrarPaciente(){
+        require_once 'views/header.inc';	
+        require_once 'views/registrarPaciente.php';        
+        require_once 'views/footer.inc';        
+    }
+    
     public function login(){
         require_once 'views/headeri.inc';	
         require_once 'views/login.php';        
@@ -66,6 +78,32 @@ class controller{
         require_once 'views/pacientes.php';                           
         require_once 'views/footer.inc';
     }
+    public function buscarPacientes(){        
+        require_once 'views/header.inc';	
+        $datosPacientes = $this->myModel->search_pacientes($_SESSION['iddoctor'],$_REQUEST['palabra']);
+        require_once 'views/pacientes.php';                                                          
+        require_once 'views/footer.inc';
+    }
+    public function agregarPaciente(){       
+       $datos = $this->myModel->add_pacientes($_SESSION['iddoctor'],$_REQUEST['nombre'],$_REQUEST['paterno'],$_REQUEST['materno'],$_REQUEST['tel'],$_REQUEST['email'],$_REQUEST['nimss'],$_REQUEST['fechan'],$_REQUEST['curp'],$_REQUEST['sangre'],$_REQUEST['alergias'],$_REQUEST['enfermedades']);
+       if($datos > 0){
+           require_once 'views/header.inc';
+           echo '<div class="container"><div class="alert alert-success fade in">'.
+                      '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.
+                       '<strong>Operacion exitosa!</strong> paciente registrado .'.
+                  '</div></div>';
+        $myController = new controller();
+        $myController->listadoPacientes();	
+	
+	//echo '<div class="alert alert-dismissible alert-danger">  <button type="button" class="close" data-dismiss="alert">&times;</button>  <strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.</div>';
+	}else{
+	//echo '<div class="alert alert-dismissible alert-danger">  <button type="button" class="close" data-dismiss="alert">&times;</button><strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.</div>';
+        $myController = new controller();
+        $myController->listadoPacientes();	
+	echo "ERROR";
+
+	}
+    }
     public function agregarDoctor(){      
        $datos = $this->myModel->add_doctor($_REQUEST['nombre'],$_REQUEST['paterno'],$_REQUEST['materno'],$_REQUEST['tel'],$_REQUEST['email'],$_REQUEST['pass'],$_REQUEST['cedula'],$_REQUEST['fechan'],$_REQUEST['curp']);
        if($datos > 0){
@@ -82,6 +120,103 @@ class controller{
                        '<strong>ERROR!</strong> Ocurrio un error, intentelo de nuevo.'.
                   '</div>';
 	}
+    }
+    public function detallePaciente(){        
+        $_SESSION['idpaciente'] = $_REQUEST['idpaciente'];
+        require_once 'views/header.inc';	
+        $datosPacientes = $this->myModel->get_datos_paciente($_SESSION['idpaciente']);
+        require_once 'views/detallePaciente.php';                           
+        $myController = new controller();
+        $myController->cons();
+        
+    }
+    public function cons(){
+        $consultas = $this->myModel->get_datos_consultas($_SESSION['idpaciente']);
+        require_once 'views/detallePacienteConsultas.php';                        	
+        require_once 'views/footer.inc';
+    }
+
+    public function formEditarPaciente(){        
+        require_once 'views/header.inc';	        
+        $datosPacientes = $this->myModel->get_datos_paciente($_SESSION['idpaciente']);        
+        require_once 'views/editPaciente.php';	        
+        require_once 'views/footer.inc';
+    }
+    public function formEditarDoctor(){        
+        require_once 'views/header.inc';	        
+        //$datosPacientes = $this->myModel->get_datos_paciente($_SESSION['idpaciente']);        
+        require_once 'views/editDoctor.php';	        
+        require_once 'views/footer.inc';        
+    }
+    public function editarPaciente(){                
+        $datos = $this->myModel->update_paciente($_SESSION['idpaciente'],$_REQUEST['nombre'],$_REQUEST['paterno'],$_REQUEST['materno'],$_REQUEST['tel'],$_REQUEST['email'],$_REQUEST['nimss'],$_REQUEST['fechan'],$_REQUEST['curp'],$_REQUEST['sangre'],$_REQUEST['alergias'],$_REQUEST['enfermedades']);        
+        if($datos > 0){
+        require_once 'views/header.inc';
+	echo '<div class="container"><div class="alert alert-success fade in">'.
+                      '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.
+                       '<strong>Operacion exitosa!</strong> cambios realizados correctamente .'.
+                  '</div></div>';           
+        $myController = new controller();
+        $myController->listadoPacientes();	
+
+	}else{
+	echo '<div class="alert alert-danger fade in">'.
+                      '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.
+                       '<strong>ERROR!</strong> Ocurrio un error, intentelo de nuevo.'.
+                  '</div>';
+	}
+    }
+    public function eliminarPaciente(){        
+//        echo $_REQUEST['idpaciente'];
+        $datos = $this->myModel->delete_paciente($_REQUEST['idpaciente']);
+        if($datos > 0){
+        require_once 'views/header.inc';    
+        echo '<div class="container"><div class="alert alert-success fade in">'.
+                      '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.
+                       '<strong>Operacion exitosa!</strong> Usuario eliminado .'.
+                  '</div></div>';            
+         $myController = new controller();
+        $myController->listadoPacientes();	
+
+	}else{
+	echo '<div class="alert alert-danger fade in">'.
+                      '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.
+                       '<strong>ERROR!</strong> Ocurrio un error, intentelo de nuevo.'.
+                  '</div>';
+	}
+        require_once 'views/footer.inc';
+    }
+    public function formConsulta(){
+        $_SESSION['idpaciente'] = $_REQUEST['idpaciente'];
+        require_once 'views/header.inc';	
+        require_once 'views/formConsulta.php';        
+        require_once 'views/footer.inc';        
+    }
+    public function agregarConsulta(){
+        $datos = $this->myModel->add_consulta($_SESSION['idpaciente'],$_REQUEST['peso'],$_REQUEST['estatura'],$_REQUEST['temperatura'],$_REQUEST['observaciones'],$_REQUEST['tratamiento']);
+        if($datos > 0){
+            require_once 'views/header.inc';
+            echo '<div class="container"><div class="alert alert-success fade in">'.
+                      '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.
+                       '<strong>Operacion exitosa!</strong> Documento generado .'.
+                  '</div></div>';
+            $datosReport = $this->myModel->get_data_report($_SESSION['iddoctor'],$_SESSION['idpaciente']);            
+            
+            echo "<div class='container'>";
+            require_once 'views/generateConsultaPDF.php';  
+            echo '<embed src="views/recent.pdf" width="100%" height="475"><br/>';            
+            echo '<center> <a href="index.php?c=controller&a=listadoPacientes2&; ?" class="btn btn-primary">REGRESAR</a></center>';
+            echo "</div>";
+            require_once 'views/footer.inc';
+	}else{
+	echo '<div class="alert alert-danger fade in">'.
+                      '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.
+                       '<strong>ERROR!</strong> Ocurrio un error, intentelo de nuevo.'.
+                  '</div>';
+	}             
+    }
+    public function borradorPDF(){
+        require_once 'views/borrador.php';          
     }
 }
 ?>
